5282e2d57328cecb804d28da6fde0a45aec73002
duythanhphan <duyth.phan@gmail.com>
2013-12-17 18:23:24 +0700

    Refac: simplify my_test

--- my_test.c
+++ my_test.c
@@ -26,64 +26,6 @@ static int test_assert( int correct, char *test )
              if( test_errors) return;               \
         } while (0)
 
-static int unhexify(unsigned char *obuf, const char *ibuf)
-{
-    unsigned char c, c2;
-    int len = strlen(ibuf) / 2;
-    assert(!(strlen(ibuf) %1)); // must be even number of bytes
-
-    while (*ibuf != 0)
-    {
-        c = *ibuf++;
-        if( c >= '0' && c <= '9' )
-            c -= '0';
-        else if( c >= 'a' && c <= 'f' )
-            c -= 'a' - 10;
-        else if( c >= 'A' && c <= 'F' )
-            c -= 'A' - 10;
-        else
-            assert( 0 );
-
-        c2 = *ibuf++;
-        if( c2 >= '0' && c2 <= '9' )
-            c2 -= '0';
-        else if( c2 >= 'a' && c2 <= 'f' )
-            c2 -= 'a' - 10;
-        else if( c2 >= 'A' && c2 <= 'F' )
-            c2 -= 'A' - 10;
-        else
-            assert( 0 );
-
-        *obuf++ = ( c << 4 ) | c2;
-    }
-
-    return len;
-}
-
-static void hexify(unsigned char *obuf, const unsigned char *ibuf, int len)
-{
-    unsigned char l, h;
-
-    while (len != 0)
-    {
-        h = (*ibuf) / 16;
-        l = (*ibuf) % 16;
-
-        if( h < 10 )
-            *obuf++ = '0' + h;
-        else
-            *obuf++ = 'a' + h - 10;
-
-        if( l < 10 )
-            *obuf++ = '0' + l;
-        else
-            *obuf++ = 'a' + l - 10;
-
-        ++ibuf;
-        len--;
-    }
-}
-
 
 void test_suite_des_encrypt_ecb( char *hex_key_string, char *hex_src_string, char *hex_dst_string ) {
     unsigned char key_str[100];
@@ -149,6 +91,8 @@ int main(int argc, char **argv) {
 	printf("Key  = %s\n", key);
  	printf("Data = %s\n", data);
 
+ 	// if length of data > 8, plit it and encode each part
+ 	// if remaining part is not fit 8 char, add '^' to fit.
  	for (i = 0; i < 8 - remaining; i++) {
 		strcat(data, "^");
 	}
@@ -157,27 +101,39 @@ int main(int argc, char **argv) {
  	for (i = 0; i < times_doing; i++) {
  		memset(data_tmp, 0x00, 10);
 		memcpy(data_tmp, &data[i*8], 8);
+
+		// encode data
 		test_suite_des_encrypt_ecb(key, data_tmp, output);
+
+		// get ciphertext (encrypted data)
 		strcat(encrypted_data, output);
 	}
 
-//	printf("Enc  = ");
-//	for (i = 0; i < len; i++) {
-//		printf("%02X ", (unsigned char) encrypted_data[i]);
-//	}
-//	printf("\n");
+ 	// print encrypted data for debugging, some value of encrypted data is not
+ 	// readable, so print it as hex format.
+	printf("Enc  = ");
+	for (i = 0; i < len; i++) {
+		printf("%02X ", (unsigned char) encrypted_data[i]);
+	}
+	printf("\n");
 
  	for (i = 0; i < times_doing; i++) {
  		memset(data_tmp, 0x00, 10);
 		memcpy(data_tmp, &encrypted_data[i*8], 8);
+
+		// decode data
 		test_suite_des_decrypt_ecb(key, data_tmp, output);
+
+		// get plaintext (decrypted data)
 		strcat(decrypted_data, output);
 	}
 
+ 	// remove '^' character which added before
  	i = strlen(decrypted_data) - 1;
  	while (decrypted_data[i] == '^') i--;
  	decrypted_data[i+1] = '\0';
 
+ 	// print decrypted data to console
 	printf("Dec  = %s\n", decrypted_data);
 
 	return 0;
